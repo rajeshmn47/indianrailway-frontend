06e4417953b360700a4cbefb256f7a0d309a46d7
rajeshmn47 <rajeshmn47@gmail.com>
2023-07-09 15:34:32 +0530

    fixing timelineview

--- components/traindetail/index.js
+++ components/traindetail/index.js
@@ -9,15 +9,22 @@ import { filterstations } from "@/utils/filtering";
 export function TrainDetail({ data }) {
   const [from, setFrom] = useState();
   const [to, setTo] = useState();
-  const [trains, setTrains] = useState([]);
+  const [train, setTrain] = useState([]);
   const [stations, setStations] = useState([]);
   console.log(data, "data");
   useEffect(() => {
     setStations(data[0].stations);
+    setTrain(data[0]);
   }, [data]);
   return (
     <>
       <div className="table timelinecontainer">
+        <div className="traindetail">
+          <p>
+            {train.trainName} ({train.trainNumber}) Train Time Table
+          </p>
+          <p>DAILY</p>
+        </div>
         <div className="tableheader">
           <div>Arrival</div>
           <div>Station</div>
